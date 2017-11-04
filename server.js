fa3eb893f84e97af77935c3538d5c146141140ff
bvasic <vasicbie@gmail.com>
2017-11-04 12:24:01 +0000

    server with POST taking json

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var server = express();
+var bodyParser = require('body-parser');
+var port = process.env.port || 1000;
+var jsonfile = require('jsonfile');
+var filesave = require('fs');
+var file = './data/data.json';
+
+jsonfile.readFile(file, function(err,obj){
+    console.log(obj);
+});
+
+server.use(bodyParser.json());
+server.post("/",function(req,res){
+        var jsonRequest = req.body;
+        jsonfile.writeFile(file,JSON.stringify(jsonRequest.person));
+});
+
+server.get("/",function(req,res){
+    jsonfile.readFile(file, function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        var content = data;
+        res.json(content);
+    });    
+});
+
+server.listen(port, function(){
+    console.log("Server running on port "+port);
+});
